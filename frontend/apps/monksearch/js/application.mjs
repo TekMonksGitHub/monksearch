d7c804e16195b496d57ec02b7670648a2487062f
Rohit Kapoor <rvkapoor@tekmonks.com>
2019-03-30 13:33:08 -0400

    Fix a refresh bug in securityguard.

--- frontend/apps/monksearch/js/application.mjs
+++ frontend/apps/monksearch/js/application.mjs
@@ -12,7 +12,7 @@ const init = async _ => {
 	window.LOG = (await import ("/framework/js/log.mjs")).LOG;
 	if (!session.get($$.MONKSHU_CONSTANTS.LANG_ID)) session.set($$.MONKSHU_CONSTANTS.LANG_ID, "en");
 	securityguard.setPermissionsMap(APP_CONSTANTS.PERMISSIONS_MAP);
-	securityguard.setCurrentRole(APP_CONSTANTS.GUEST_ROLE);
+	securityguard.setCurrentRole(securityguard.getCurrentRole() || APP_CONSTANTS.GUEST_ROLE);
 }
 
 const main = async _ => {

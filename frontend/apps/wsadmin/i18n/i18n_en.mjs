6f02f7ea51c1aa2e9a172d7aa62c8edcd756338b
Rohit Kapoor <rvkapoor@tekmonks.com>
2020-01-05 04:47:26 -0500

    Initial commit of code.

---
+++ frontend/apps/wsadmin/i18n/i18n_en.mjs
@@ -0,0 +1,14 @@
+export const i18n = {
+"Title" : "WebScrolls Admin",
+"Logout" : "Logout",
+"Back" : "Back",
+"UserID" : "User ID",
+"DesiredUserID" : "Desired User ID",
+"Password" : "Password",
+"SignIn" : "Login",
+"BadLogin" : "Incorrect Login",
+"Register" : "Register",
+"LoginTaken" : "Login ID taken",
+"PasswordTooShort" : "Password too short",
+"Search": "Search"
+}
---
+++ frontend/apps/wsadmin/i18n/i18n_hi.mjs
@@ -0,0 +1,14 @@
+export const i18n = {
+"Title" : "वेबसक्रोल्ल्स कंट्रोल करें",
+"Logout" : "बाहर निकलें",
+"Back" : "वापस",
+"UserID" : "आपकी आईडी",
+"DesiredUserID" : "नई आईडी",
+"Password" : "पासवर्ड",
+"SignIn" : "लॉग इन करें",
+"BadLogin" : "गलत लॉग इन",
+"Register" : "रजिस्टर करें",
+"LoginTaken" : "आईडी उपलब्ध नहीं,कुछ और चुनें",
+"PasswordTooShort" : "पासवर्ड बहुत छोटा है",
+"Search":"खोजिए"
+}
